e9b5d7adccf637c3bf2f71a3d935c5e83b46b373
Kazi Jamal <jamalkazi02@gmail.com>
2020-04-16 15:16:12 -0400

    Add gradient cards and animation

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    shape = document.getElementById("shape");
+    shape.style.strokeWidth = "15px";
+    shape.style.strokeDashoffset = 0;
+    shape.style.strokeDasharray = 1000;
+});
